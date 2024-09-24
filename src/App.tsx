9340a67702cac35488e86303c99ca3e29c09a302
ALIA-HAIDER <turabhaider786@outlook.com>
2024-09-24 15:41:34 +0530

    footer done

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,19 @@ import { Outlet } from "react-router-dom";
 import { NextUIProvider } from "@nextui-org/react";
 import NavBar from "./NavBar/NavBar";
 import Footer from "./Footer/Footer";
+import './App.css'
+
 
 function App() {
   return (
     <>
-      <NextUIProvider>
-        <NavBar />
-        <Outlet />
-        <Footer />
-      </NextUIProvider>
+      <div className="landingPage">
+        <NextUIProvider>
+          <NavBar />
+          <Outlet />
+          <Footer />
+        </NextUIProvider>
+      </div>
     </>
   );
 }
