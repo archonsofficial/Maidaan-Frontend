f81362d50a667994e0bbfd8af07a9a911ef1374d
Mayank Gupta <mayankg88813@gmail.com>
2024-10-13 21:47:08 +0530

    Font Correction

--- src/Components/LpEventPage/LpEvent.tsx
+++ src/Components/LpEventPage/LpEvent.tsx
@@ -1,8 +1,7 @@
-import Eone from "../../assets/Eone.jpeg"
-import Etwo from "../../assets/Etwo.jpeg"
-import Ethree from "../../assets/Ethree.jpeg"
+import Eone from "../../assets/Eone.jpeg";
+import Etwo from "../../assets/Etwo.jpeg";
+import Ethree from "../../assets/Ethree.jpeg";
 import BTnCompo from "../Btn/BTnCompo";
-
 
 export default function LpEvent() {
   const eventData = [
@@ -11,71 +10,84 @@ export default function LpEvent() {
       date: "Oct 10, 2024",
       location: "Serenity, Mumbai",
       title: "Finding Your Inner Peace",
-      description: "Experience rejuvenation and tranquility through guided yoga sessions.",
+      description:
+        "Experience rejuvenation and tranquility through guided yoga sessions.",
     },
     {
       imgUrl: Etwo,
       date: "Oct 10, 2024",
       location: "Serenity, Mumbai",
       title: "Move to the Rhythm",
-      description: "Dance your heart out while enjoying a high-energy Zumba workout.",
+      description:
+        "Dance your heart out while enjoying a high-energy Zumba workout.",
     },
     {
       imgUrl: Ethree,
       date: "Oct 10, 2024",
       location: "Serenity, Mumbai",
       title: "Strength in Diversity",
-      description: "Showcase strength and determination in an inclusive weightlifting competition.",
+      description:
+        "Showcase strength and determination in an inclusive weightlifting competition.",
     },
-
   ];
 
   return (
     <section className=" text-white py-12 m-8">
       <div className="container mx-auto">
-      <div className="flex justify-start items-end mb-8">
-          <button className="linkbtn text-[#CEF23F] bg-[#FFFFFF10] px-4 py-[10px] rounded-full text-xs  font-bold h-9 flex items-center">
-           Events
+        <div className="flex justify-start items-end mb-8">
+          <button className="linkbtn text-[#CEF23F] bg-[#FFFFFF10] px-4 py-[10px] rounded-full text-sm  font-bold h-9 font-secondaryFont flex items-center">
+            EVENTS
           </button>
         </div>
 
         <h2 className="text-4xl font-normal  ">
-        Discover Thrilling Sports Events
+          Discover Thrilling Sports Events
         </h2>
-        
 
         {/* <BTnCompo text="View All Events"/> */}
-        <div className="flex justify-end items-end ">
-          <BTnCompo text="View All Events"/>
+        <div className="flex justify-end items-end mb-8 ">
+          <BTnCompo text="View All Events" />
         </div>
 
-      <div className="maintwo  mx-auto grid gap-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 lg:grid-rows-1 justify-center mb-16 lg:mb-44">
-        {eventData.map((event, index) => (
-          <div key={index} className="card flex flex-col justify-center align-middle items-center">
+        <div className="maintwo  mx-auto grid gap-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 lg:grid-rows-1 justify-center mb-16 lg:mb-44">
+          {eventData.map((event, index) => (
             <div
-              className="imgpart items-end bg-cover flex h-72 md:h-[350px] w-72 md:w-[20rem] rounded-t-[15%]"
-              style={{ backgroundImage: `url(${event.imgUrl})` }}
+              key={index}
+              className="card flex flex-col justify-center align-middle items-center"
             >
-              
-              <div className="btnpart flex justify-evenly h-16 md:h-20 w-full items-center bg-black/40">
-                <div className="data flex items-center justify-evenly w-28 md:w-36 h-8 bg-black/40 rounded-2xl">
-                  <i className="fa-regular fa-calendar-days fa-sm" style={{ color: "#cef23f" }}></i>
-                  <p className="text-xs">{event.date}</p>
-                </div>
-                <div className="data flex items-center justify-evenly w-28 md:w-36 h-8 bg-black/40 rounded-2xl">
-                  <i className="fa-solid fa-location-dot fa-sm" style={{ color: "#cef23f" }}></i>
-                  <p className="text-xs">{event.location}</p>
+              <div
+                className="imgpart items-end bg-cover flex h-72 md:h-[350px] w-72 md:w-[20rem] rounded-t-[15%]"
+                style={{ backgroundImage: `url(${event.imgUrl})` }}
+              >
+                <div className="btnpart flex justify-evenly h-16 md:h-20 w-full items-center bg-black/40">
+                  <div className="data flex items-center justify-evenly w-28 md:w-36 h-8 bg-black/40 rounded-2xl">
+                    <i
+                      className="fa-regular fa-calendar-days fa-sm"
+                      style={{ color: "#cef23f" }}
+                    ></i>
+                    <p className="text-xs">{event.date}</p>
+                  </div>
+                  <div className="data flex items-center justify-evenly w-28 md:w-36 h-8 bg-black/40 rounded-2xl">
+                    <i
+                      className="fa-solid fa-location-dot fa-sm"
+                      style={{ color: "#cef23f" }}
+                    ></i>
+                    <p className="text-xs">{event.location}</p>
+                  </div>
                 </div>
               </div>
+              <div className="biopart bg-[#FFFFFF0D] flex flex-col  w-64 md:w-[18rem] p-4 rounded-2xl mt-2">
+                <h1 className="text-[#CEF23F] text-left justify-start">
+                  {event.title}
+                </h1>
+                <h2 className="text-xs md:text-sm font-thin">
+                  {event.description}
+                </h2>
+              </div>
             </div>
-            <div className="biopart bg-[#FFFFFF0D] flex flex-col  w-64 md:w-[18rem] p-4 rounded-2xl mt-2">
-              <h1 className="text-[#CEF23F] text-left justify-start">{event.title}</h1>
-              <h2 className="text-xs md:text-sm font-thin">{event.description}</h2>
-            </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
     </section>
   );
 }
--- src/Components/LpTournamentPage/LpTournaments.tsx
+++ src/Components/LpTournamentPage/LpTournaments.tsx
@@ -6,7 +6,6 @@ import Tthree from "../../assets/Tthree.jpeg";
 import Tfour from "../../assets/Tfour.jpeg";
 import BTnCompo from "../Btn/BTnCompo";
 
-
 // Sample data
 const tournaments = [
   {
@@ -34,59 +33,56 @@ const tournaments = [
 export default function LpTournaments() {
   return (
     <section className=" text-white py-12 m-8">
-    <div className="container mx-auto">
-    <div className="flex justify-start items-end mb-8">
-        <button className="linkbtn text-[#CEF23F] bg-[#FFFFFF10] px-4 py-[10px] rounded-full text-xs  font-bold h-9  flex items-center">
-         Tournaments
-        </button>
-      </div>
-      <h2 className="text-4xl font-normal  ">
-        Explore Our Tournaments
-      </h2>
-      <div className="flex justify-end items-end mb-8">
-        <button className="linkbtn bg-[#CEF23F] hover:bg-lime-500 text-black px-4 py-2 rounded-full text-xs md:text-sm font-light w-50 md:w-50 h-7 flex items-center">
-          View All Tournaments
-        </button>
+      <div className="container mx-auto">
+        <div className="flex justify-start items-end mb-8">
+          <button className="linkbtn text-[#CEF23F] bg-[#FFFFFF10] px-4 py-[10px] rounded-full text-sm  font-bold h-9 font-secondaryFont flex items-center">
+            TOURNAMENTS
+          </button>
+        </div>
+        <h2 className="text-4xl font-normal  ">Explore Our Tournaments</h2>
+        <div className="flex justify-end items-end mb-8">
+          <button className="linkbtn bg-[#CEF23F] hover:bg-lime-500 text-black px-4 py-2 rounded-full text-xs md:text-sm font-light w-50 md:w-50 h-7 flex items-center">
+            View All Tournaments
+          </button>
         </div>
 
-    
-      {/* table content */}
-      <div className="max-w-[1000px] gap-4 grid grid-cols-12 grid-rows-2 px-8 mb-9">
-        {tournaments.map((tournament, index) => (
-          <Card
-            key={index}
-            className={`w-full h-[330px] ${
-              index === 0 || index === 3
-                ? "col-span-12 sm:col-span-7"
-                : "col-span-12 sm:col-span-5"
-            }`}
-          >
-            <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-              <h4 className="text-white font-medium text-large">
-                {tournament.title}
-              </h4>
-            </CardHeader>
-            <Image
-              removeWrapper
-              alt="Card background"
-              className="z-0 w-full h-full object-cover bg-white/30 "
-              src={tournament.image}
-            />
-            <div className="absolute inset-0 bg-black bg-opacity-50"></div>
-            <CardFooter className="absolute bg-white/30 bottom-0 border-t-1 border-zinc-100/50 z-10 justify-between">
-              <div>
-                <p className="text-white text-tiny">{tournament.location}</p>
-              </div>
-              <Button
-                className="text-tiny bg-[#CEF23F] rounded-full text-black"
-                size="sm"
-              >
-                Register Now
-              </Button>
-            </CardFooter>
-          </Card>
-        ))}
-      </div>
+        {/* table content */}
+        <div className="max-w-[1000px] gap-4 grid grid-cols-12 grid-rows-2 px-8 mb-9">
+          {tournaments.map((tournament, index) => (
+            <Card
+              key={index}
+              className={`w-full h-[330px] ${
+                index === 0 || index === 3
+                  ? "col-span-12 sm:col-span-7"
+                  : "col-span-12 sm:col-span-5"
+              }`}
+            >
+              <CardHeader className="absolute z-10 top-1 flex-col !items-start">
+                <h4 className="text-white font-medium text-large">
+                  {tournament.title}
+                </h4>
+              </CardHeader>
+              <Image
+                removeWrapper
+                alt="Card background"
+                className="z-0 w-full h-full object-cover bg-white/30 "
+                src={tournament.image}
+              />
+              <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+              <CardFooter className="absolute bg-white/30 bottom-0 border-t-1 border-zinc-100/50 z-10 justify-between">
+                <div>
+                  <p className="text-white text-tiny">{tournament.location}</p>
+                </div>
+                <Button
+                  className="text-tiny bg-[#CEF23F] rounded-full text-black"
+                  size="sm"
+                >
+                  Register Now
+                </Button>
+              </CardFooter>
+            </Card>
+          ))}
+        </div>
       </div>
     </section>
   );
